85f4e10c572a5784f16c01666006f71c504cd4aa
Soraly <lequangnam1617@gmail.com>
2021-05-01 07:15:35 +0700

    BetweenThreeNumber

--- ShortenCode/BetweenThreeNumber/index.js
+++ ShortenCode/BetweenThreeNumber/index.js
@@ -16,7 +16,24 @@
 // 1
 
 //Solution
-
+// Short
 N=+readline();a=[];for(i=0;i<N;i++) {s=readline().split(' ');X=+s[0];Y=+s[1];Z=+s[2];a.push(X,Y,Z);a.sort((x,y)=>x-y); print(a[1]);a.splice(0,3)}
+
+// Full
+const N=parseInt(readline());
+let a=[];
+for(let i=0;i<N;i++) 
+{
+    s=readline().split(' ');
+    X=parseInt(s[0]);
+    Y=parseInt(s[1]);
+    Z=parseInt(s[2]);
+    a.push(X,Y,Z);
+    a.sort((x,y)=>x-y); 
+    console.log(a[1]);
+    a.splice(0,3);
+}
+
+
 
 // Pass 100% unit tests
